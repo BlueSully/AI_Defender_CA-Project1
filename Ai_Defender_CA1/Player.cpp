d0c101d995b61ff9adc6786ca2221a3fc37e237b
AlexMounsey <Alex.w.Mounsey@gmail.com>
2016-12-09 13:05:10 +0000

    Hyperjump done, pickUp to be added

--- Ai_Defender_CA1/Player.cpp
+++ Ai_Defender_CA1/Player.cpp
@@ -1,5 +1,6 @@
 #include "Player.h"
 #include <iostream>
+
 Player::Player()
 : m_velocity(sf::Vector2f(0, 0)),
   m_size(sf::Vector2f(50, 30)),
@@ -12,6 +13,8 @@ Player::Player()
 	m_boundingBox.setSize(m_size);
 	m_playerState = PlayerStates::IDLE;
 	m_speed = sf::Vector2f(m_MAXHORIZONTALACCLERATION / 2, m_MAXVERTICALACCLERATION / 2);
+	srand(time(NULL));
+	m_jumpReady = true;
 }
 
 Player::~Player()
@@ -39,7 +42,8 @@ void Player::processInputs(sf::Event *evt)
 	}
 
 	if (!sf::Keyboard::isKeyPressed(sf::Keyboard::A) && !sf::Keyboard::isKeyPressed(sf::Keyboard::D) &&
-		!sf::Keyboard::isKeyPressed(sf::Keyboard::W) && !sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+		!sf::Keyboard::isKeyPressed(sf::Keyboard::W) && !sf::Keyboard::isKeyPressed(sf::Keyboard::S)
+		&&!sf::Keyboard::isKeyPressed(sf::Keyboard::Q))
 	{
 		m_playerState = PlayerStates::IDLE;
 	}
@@ -48,6 +52,12 @@ void Player::processInputs(sf::Event *evt)
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
 	{
 		std::cout << "Fire Laser" << std::endl;
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q))
+	{
+
+
+		m_playerState = PlayerStates::HYPERJUMP;
 	}
 }
 
@@ -63,16 +73,16 @@ void Player::setPosition(sf::Vector2f pos)
 
 void Player::boundaryResponse(sf::Vector2f worldSize)
 {
-	sf::Vector2f vect = sf::Vector2f(worldSize.x, worldSize.y);
+	m_worldSize = sf::Vector2f(worldSize.x, worldSize.y);
 
 	if(getPosition().y + m_size.y < m_size.y)
 	{
 		setPosition(sf::Vector2f(getPosition().x, 0));
 		m_velocity.y = 0;
 	}
-	else if (getPosition().y + m_size.y > vect.y)
+	else if (getPosition().y + m_size.y > m_worldSize.y)
 	{
-		setPosition(sf::Vector2f(getPosition().x, vect.y - m_size.y));
+		setPosition(sf::Vector2f(getPosition().x, m_worldSize.y - m_size.y));
 		m_velocity.y = 0;
 	}
 }
@@ -87,7 +97,6 @@ void Player::update(sf::Time deltaTime)
 	{
 		m_velocity.x -= m_speed.x * deltaTime.asSeconds();
 	}
-
 	if (m_playerState == PlayerStates::MOVING_UP && m_velocity.y > -m_MAXVERTICALACCLERATION)
 	{
 		m_velocity.y -= m_speed.y * deltaTime.asSeconds();
@@ -96,7 +105,6 @@ void Player::update(sf::Time deltaTime)
 	{
 		m_velocity.y += m_speed.y * deltaTime.asSeconds();
 	}
-
 	if (m_playerState == PlayerStates::IDLE) 
 	{
 		if ((m_velocity.x > 0 && m_velocity.x < 0.1f) || (m_velocity.x < 0 && m_velocity.x > -0.1f))
@@ -125,7 +133,13 @@ void Player::update(sf::Time deltaTime)
 			m_velocity.y += m_speed.y * deltaTime.asSeconds();
 		}
 	}
-
+	if (m_playerState == PlayerStates::HYPERJUMP && m_jumpReady ==true)
+	{
+		int worldX = -(m_worldSize.x / 2);
+		int worldY = m_worldSize.y;
+		setPosition(sf::Vector2f(rand() % ((int)m_worldSize.x+1) +(worldX), rand() % (worldY-50) +50));
+		m_jumpReady = false;
+	}
 	m_boundingBox.move(m_velocity * deltaTime.asSeconds());
 }
 
--- Ai_Defender_CA1/Player.h
+++ Ai_Defender_CA1/Player.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <SFML\Graphics.hpp>
 #include "PlayerState.h"
+#include <time.h>
 
 class Player
 {
@@ -8,8 +9,12 @@ private:
 	sf::Vector2f m_velocity;
 	sf::Vector2f m_size;
 	sf::Vector2f m_speed;
+	sf::Vector2f m_worldSize;
 	const float m_MAXHORIZONTALACCLERATION;
 	const float m_MAXVERTICALACCLERATION;
+	bool m_smartBombReady;
+	bool m_jumpReady;
+	float timer;
 
 	sf::RectangleShape m_boundingBox;
 	PlayerStates m_playerState;
