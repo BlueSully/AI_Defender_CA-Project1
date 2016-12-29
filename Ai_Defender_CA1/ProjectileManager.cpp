afba53251ad2cc67eeb36b44fdd422fca73ea0d9
AlexMounsey <Alex.w.Mounsey@gmail.com>
2016-12-29 13:11:30 +0000

    ProjectileManager hotfix

--- Ai_Defender_CA1/ProjectileManager.cpp
+++ Ai_Defender_CA1/ProjectileManager.cpp
@@ -18,12 +18,12 @@ void ProjectileManager::Update(sf::Time deltaTime)
 
 void ProjectileManager::addLaser(bool isLeft, sf::Vector2f playerPos, float playerXVelocity, int ttl)
 {
-
+	m_projectiles.push_back(Projectile(isLeft, playerPos, playerXVelocity, ttl)); 
 }
 
 void ProjectileManager::addMissile(sf::Vector2f playerPos, sf::Vector2f nestPos, int ttl)
 {
-
+	m_projectiles.push_back(Projectile(playerPos, nestPos, ttl));
 }
 
 void ProjectileManager::Render(sf::RenderWindow & renderer)
